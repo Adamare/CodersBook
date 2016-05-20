49e5c5fc753375b15b76796d105006dbde11fb5e
escees <scisly87@gmail.com>
2016-05-20 15:53:10 +0200

    Added ShowAllPersons Action

--- src/CodersLab/CodersBookBundle/Controller/PersonController.php
+++ src/CodersLab/CodersBookBundle/Controller/PersonController.php
@@ -5,7 +5,24 @@ namespace CodersLab\CodersBookBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use CodersLab\CodersBookBundle\Entity\Person;
+use CodersLab\CodersBookBundle\Entity\CLGroup;
+use Symfony\Component\HttpFoundation\Response;
 
 class PersonController extends Controller {
-    
+    /**
+     * @Route("/showAllPersons", name = "showAllPersons")
+     * @Template()
+     */
+    public function showAllPersonsAction() {
+        $repo = $this->getDoctrine()->getRepository('CodersBookBundle:Person');
+
+        $persons = $repo->findAll();
+        return [
+            'persons' => $persons,
+            
+        ];
+    }
 }
